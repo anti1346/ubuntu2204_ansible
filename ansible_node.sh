44429d5e044a069531b866f597417bb2a1b5f8db
aby <anti1346@gmail.com>
2024-02-02 00:19:56 +0900

    no message

--- ansible_node.sh
+++ ansible_node.sh
@@ -1,46 +1,42 @@
 #!/bin/bash
 
+user_name=vagrant
+
 ###################################################################
 ############################# 함수 파일 #############################
 ###################################################################
 generate_ssh_private_key() {
-    cat <<EOF > /home/vagrant/.ssh/id_rsa
+    sudo cat <<EOF > /home/$user_name/.ssh/id_rsa
 -----BEGIN OPENSSH PRIVATE KEY-----
 b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABFwAAAAdzc2gtcn
-NhAAAAAwEAAQAAAQEAzjj1aU9WovLI/dGA6Ambo2Dsn3+dBAOwSR5uUGXFzEP+u9B+OlFS
-V4jd7TE5TLv/3GimTgVOh00Qh4OQoPgg22I3jgYBgjkhJPZMTZkbBNqwoXXkBS4WgnAeeh
-CmMhyfbmKW3u9jHFlrM0wx6bXskpQaRP+2I53tg/U90y0p8Md5HSGdceCerccLcNwYkip/
-8FcXMV+SZhwH/ha+l69bJDrWNNYpCGn1l7Nd0RUcy3yL611IZnHyKcEi30YmN+rjrJEi6J
-syubpKijKPDKhR9q8WyUl1m6Qe56tVSlvvFD3lQ25QF4+/ZLp9ofKwE9U8GFBBSYnpRX5e
-NA23q9qVOQAAA8AhJ4/TISeP0wAAAAdzc2gtcnNhAAABAQDOOPVpT1ai8sj90YDoCZujYO
-yff50EA7BJHm5QZcXMQ/670H46UVJXiN3tMTlMu//caKZOBU6HTRCHg5Cg+CDbYjeOBgGC
-OSEk9kxNmRsE2rChdeQFLhaCcB56EKYyHJ9uYpbe72McWWszTDHpteySlBpE/7Yjne2D9T
-3TLSnwx3kdIZ1x4J6txwtw3BiSKn/wVxcxX5JmHAf+Fr6Xr1skOtY01ikIafWXs13RFRzL
-fIvrXUhmcfIpwSLfRiY36uOskSLomzK5ukqKMo8MqFH2rxbJSXWbpB7nq1VKW+8UPeVDbl
-AXj79kun2h8rAT1TwYUEFJielFfl40Dber2pU5AAAAAwEAAQAAAQAF3wLu+xvFKt1Ztepn
-cMfraFnZlqQClmpL7UpduUVe3JFhiSeMLdQxesuQB7E7w98ecs+dSV25e2eyO/wqS7Yqbi
-A/x7Wo8/XDcXcC/zi7iXsINcIxUDptf0IBiVIkpheh6GZRituLAJQNPf5DyvKlH635yJVH
-wU90TM9JoQULKPg6+2u50dJOgKvqocXM29MK+XRazm9yesC6f4lP7X5PzqDpaQ+Oicvf7H
-X9W3yOpE3uD8wnvsUQ7lPtMyZ2dD34q+4CqtDQnf5UPv6LJxVrPYfbLgdsNzmKLxdjqltG
-6XeZD9eWL4MAsqDZJ7wOZvhuP6XkddkGaj3bbnnQGjhpAAAAgCCs3SgG1ppxgnJcVqgHf/
-RchKxhgoq0o81YIiPvlVunYrdd9FtZyTGX2gSVRIfTRPMwEyakN/7v0Y1HxQKL43BZxJj8
-qf1edpxY7pMF0IDN0Cbq8aDU2fXgUgTLc3ZBMM8kFzMWPR9DYFKOmh97YWWLAvyWSMmMOh
-My4WnTpVGiAAAAgQD3PWqavEDIKnjwN40k/i4ulbELu2bc7pPltAcQ7U4kO5Li/DcgaYbv
-npCLMdIfgbZxiYOyy/8LI3/i5Ov334BMusg6NdH17PRKNOjI6l4X5wUKg2eY7dBo0E34k8
-6uYV/TJ1nSfp05oFR1Jh7bmH3wvgK4Xngxj/ppF9CLLNO4rQAAAIEA1Yd+qOY2A2z3eRkz
-FeJKCNixncDSgioNd8GBvm6nbh13B5c0UPWY2oSUsPDJe+jzGJ3v34cRTr797NQVq8gG1G
-DGDGdktw7XNHXxseYne0HdcMTbiVzPg65ojp7CRGOk8l2mIqCYK6/SpZD9wziWaIY4YtRI
-goGjeiD4+QLUZD0AAAAKZGVwbG95bWVudAE=
+NhAAAAAwEAAQAAAQDW0VhWwYC1X6bfM1vIwKk983SdipCWUUOrEFHmKJs2Ug9boiqzB9ZikI
+SeTXTnJccqgUuf8NntoFZWuV/X24hJEtqy6JqibJWMTIySDbJW0lp182+RU/hmJEL3tyTBy
+nKoWsAQ7ZhER+OTgwNg9FZ6OzQaVY5tL1fIqc+X96Tbk1RYtMOZRKg7NyG9hFCpB1XqkHqe
+rsERQiEpF31WTnavjHvc4WCFYdNUDDp0FHd8+Pc3NG7KPtyxIZRrAtvf6Njb/ANJXK9glqF
+bFSh6B7mmV//aoHVtk6SWalnhh97WUtK+9d+4TL0qJr7CssW9yaE8+l7E632XeNt7o7X3Kz
+HHAAAAAwEAAQAAAQATwIgI4h39j1/+ofBMkzp8kcglPDpzN+Gq9wMw0IcFBaKX4F9gutXB
+llw7Wg/nuReJFv5nJHRFXzrRGpZveKYdYfht/ulJCgbuMZOzUkMVrJ3YvpuBl2D9s1PdByA
+AGadVR5Lle5NGo/2O8Lr8zTquXoc57Kf97h56OvL1nDWyYrx1DWyhWMyp9vkkg2RH3z0+18
+dyPpZOSbCSlePjVX1xyGohWl5Y5VZ9LuAkZtFTQSi+/hperHEIP1Lm+aaZsTKlvtVKP+8Fo
+9RohK7zDihsknL1DsS4bKNgMvvi8BghThhwiR8ntNFkNcxnLm6kn1iWkE6JuqDBXq826uMD
+R1bBAAAAgQCtIW6HODQgnvaG9Z6W85wi6M9iHMIcsci4n2lJYgBoq4JT84hpFV80gCoYUnVG
+c+nsPf2/kzDKjCtAP/lmAgLrMmDMlHzvkBVJyWDv26eIHZjo6htqpIGhQWaVTw+O6PA5dRy
+k1GvL5Qqf131DGvhZcqLqDiORLzgvexrYQhzUBgAAAIEA81J7Dgis+cAVK8ugXeovxKCmN7
+pp9xOgUp5xNjst87zLSWywCO/0AqF9bCovePB3SYl8BMUqAOnb1Wai9IBExGY1LxUnWdArme
+vLejiEWBLd38+m8Yjy5Rmz6hT2p58osSDE0FuIaPMIBrENSv0RN0PIGE/te0u6DijCV6p4U
+9EAAACBAOICqXOvHoVTTKaxuBKNwKoYVj1eMTlCB3k5krc1PNtXyMM6jvyIRdKhKr7bDyYN+
+Q3wCsW/zOfQbv2J6pW///9vAGQsgm22XH95xvEidDHLbpcD2WhmDp3OLc8iIMLseHrjtjaqg
+tvyKadKBxymznQWjmplTyFO6a9DU9HCnYoXAAAACmRlcGxveW1lbnQ=
 -----END OPENSSH PRIVATE KEY-----
 EOF
-    chmod 600 /home/vagrant/.ssh/id_rsa
+    sudo chmod 600 /home/$user_name/.ssh/id_rsa
 }
 
 add_ssh_authorized_key() {
-    cat <<EOF >> /home/vagrant/.ssh/authorized_keys
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOOPVpT1ai8sj90YDoCZujYOyff50EA7BJHm5QZcXMQ/670H46UVJXiN3tMTlMu//caKZOBU6HTRCHg5Cg+CDbYjeOBgGCOSEk9kxNmRsE2rChdeQFLhaCcB56EKYyHJ9uYpbe72McWWszTDHpteySlBpE/7Yjne2D9T3TLSnwx3kdIZ1x4J6txwtw3BiSKn/wVxcxX5JmHAf+Fr6Xr1skOtY01ikIafWXs13RFRzLfIvrXUhmcfIpwSLfRiY36uOskSLomzK5ukqKMo8MqFH2rxbJSXWbpB7nq1VKW+8UPeVDblAXj79kun2h8rAT1TwYUEFJielFfl40Dber2pU5 deployment
+    sudo cat <<EOF >> /home/$user_name/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDW0VhWwYC1X6bfM1vIwKk983SdipCWUUOrEFHmKJs2Ug9boiqzB9ZikISeTXTnJccqgUuf8NntoFZWuV/X24hJEtqy6JqibJWMTIySDbJW0lp182+RU/hmJEL3tyTBynKoWsAQ7ZhER+OTgwNg9FZ6OzQaVY5tL1fIqc+X96Tbk1RYtMOZRKg7NyG9hFCpB1XqkHqersERQiEpF31WTnavjHvc4WCFYdNUDDp0FHd8+Pc3NG7KPtyxIZRrAtvf6Njb/ANJXK9glqFbFSh6B7mmV//aoHVtk6SWalnhh97WUtK+9d+4TL0qJr7CssW9yaE8+l7E632XeNt7o7X3KzHH deployment
 EOF
-    chmod 400 /home/vagrant/.ssh/authorized_keys
+    sudo chmod 400 /home/$user_name/.ssh/authorized_keys
 }
 ###################################################################
 ############################# 함수 파일 #############################
@@ -48,58 +44,60 @@ EOF
 
 
 
-#### COMMON
-sudo sed -i.bak "s/\(kr\|archive\).ubuntu.com/mirror.kakao.com/g" /etc/apt/sources.list
-sudo apt-get update
-
 # 필수 패키지 설치
-if ! dpkg -l | grep -q openssh-server; then
-    sudo apt-get install -y lsb-release ca-certificates rsyslog openssh-server vim
-    sudo systemctl --now enable rsyslog
-    sudo systemctl --now enable ssh.service
-    sudo sed -i.bak 's/^#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
-    sudo systemctl restart ssh.service
-fi
-
-# tzdata, chrony 패키지 설치
-if ! dpkg -l | grep -q chrony; then
-    sudo echo "Asia/Seoul" | sudo tee /etc/timezone
-    sudo apt-get install -y tzdata chrony
-    sudo timedatectl set-timezone Asia/Seoul
-    sudo systemctl --now enable chrony
+if ! dpkg -l | grep -q lsb-release; then
+    sudo apt-get install -y lsb-release ca-certificates vim
 fi
 
 # vagrant 계정이 존재하지 않으면 vagrant_useradd.sh 스크립트 실행
-if ! id "vagrant" &>/dev/null; then
+if ! id "$user_name" &>/dev/null; then
     curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/vagrant_useradd.sh | sudo bash
 fi
 
 if [ "$HOSTNAME" == "ansible" ]; then
     #### ansible
-    #ssh-keygen -t rsa -b 2048 -C "deployment" -f /home/vagrant/.ssh/id_rsa -N ""
-    if [ ! -f "/home/vagrant/.ssh/id_rsa" ]; then
-        mkdir -m 700 /home/vagrant/.ssh
+    sudo timedatectl set-timezone Asia/Seoul
+
+    #ssh-keygen -t rsa -b 2048 -C "deployment" -f /home/$user_name/.ssh/id_rsa -N ""
+    if [ ! -f "/home/$user_name/.ssh/id_rsa" ]; then
+        sudo mkdir -m 700 /home/$user_name/.ssh
         # 함수 호출하여 id_rsa 파일 생성
         generate_ssh_private_key
-        echo "$(whoami):$(whoami)" | chpasswd
     fi
 fi
 
 if [ "$HOSTNAME" == "web01" -o "$HOSTNAME" == "web02" ]; then
-    #### web01, web02
-    if [ ! -f "/home/vagrant/.ssh/authorized_keys" ]; then
-        mkdir -m 700 /home/vagrant/.ssh
-        # 함수 호출하여 authorized_keys 파일에 공개 키 추가
-        add_ssh_authorized_key
-        echo "$(whoami):$(whoami)" | chpasswd
+    # 필수 패키지 설치
+    if ! dpkg -l | grep -q openssh-server; then
+        sudo apt-get install -y rsyslog openssh-server
+        sudo systemctl --now enable rsyslog
+        sudo systemctl --now enable ssh.service
+        sudo sed -i.bak 's/^#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+        sudo systemctl restart ssh.service
     fi
+
+    # tzdata, chrony 패키지 설치
+    if ! dpkg -l | grep -q chrony; then
+        sudo echo "Asia/Seoul" | sudo tee /etc/timezone
+        sudo apt-get install -y tzdata chrony
+        sudo timedatectl set-timezone Asia/Seoul
+        sudo systemctl --now enable chrony
+    fi
+
+    echo -e "\n### rsyslog status"
+    sudo systemctl status rsyslog.service
+
+    echo -e "\n### ssh status"
+    sudo systemctl status ssh.service
 fi
 
-echo -e "\n### rsyslog status"
-sudo systemctl status rsyslog.service
-
-echo -e "\n### ssh status"
-sudo systemctl status ssh.service
+if [ ! -f "/home/$user_name/.ssh/authorized_keys" ]; then
+    sudo mkdir -m 700 /home/$user_name/.ssh
+    # 함수 호출하여 authorized_keys 파일에 공개 키 추가
+    add_ssh_authorized_key
+    sudo echo "$user_name:$user_name" | chpasswd
+    sudo chown -R "$user_name:$user_name" /home/$user_name/.ssh
+fi
 
 
 ### Shell Execute Command
