006c86e0acb9c011b287b869655a93a91ecaa95a
aby <anti1346@gmail.com>
2024-02-02 00:29:12 +0900

    no message

--- ansible_node.sh
+++ ansible_node.sh
@@ -61,8 +61,7 @@ if [ "$HOSTNAME" == "ansible" ]; then
         # 함수 호출하여 id_rsa 파일 생성
         generate_ssh_private_key
     fi
-
-elif [ "$HOSTNAME" == "web01" -o "$HOSTNAME" == "web02" ]; then
+else
     # 필수 패키지 설치
     if ! dpkg -l | grep -q openssh-server; then
         sudo apt-get install -y rsyslog openssh-server
@@ -85,8 +84,6 @@ elif [ "$HOSTNAME" == "web01" -o "$HOSTNAME" == "web02" ]; then
 
     echo -e "\n### ssh status"
     sudo systemctl status ssh.service
-else
-    echo "end"
 fi
 
 if [ ! -f "/home/$user_name/.ssh/authorized_keys" ]; then
