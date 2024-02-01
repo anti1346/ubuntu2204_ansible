085258a3b331261362d89b99faa1f8e9b1ae0302
aby <anti1346@gmail.com>
2024-02-02 00:55:03 +0900

    no message

--- ansible_node.sh
+++ ansible_node.sh
@@ -36,7 +36,7 @@ add_ssh_authorized_key() {
     sudo cat <<EOF >> /home/$user_name/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDW0VhWwYC1X6bfM1vIwKk983SdipCWUUOrEFHmKJs2Ug9boiqzB9ZikISeTXTnJccqgUuf8NntoFZWuV/X24hJEtqy6JqibJWMTIySDbJW0lp182+RU/hmJEL3tyTBynKoWsAQ7ZhER+OTgwNg9FZ6OzQaVY5tL1fIqc+X96Tbk1RYtMOZRKg7NyG9hFCpB1XqkHqersERQiEpF31WTnavjHvc4WCFYdNUDDp0FHd8+Pc3NG7KPtyxIZRrAtvf6Njb/ANJXK9glqFbFSh6B7mmV//aoHVtk6SWalnhh97WUtK+9d+4TL0qJr7CssW9yaE8+l7E632XeNt7o7X3KzHH deployment
 EOF
-    sudo chmod 400 /home/$user_name/.ssh/authorized_keys
+    sudo chmod 644 /home/$user_name/.ssh/authorized_keys
 }
 ###################################################################
 ############################# 함수 파일 #############################
@@ -47,7 +47,7 @@ EOF
 # 필수 패키지 설치
 if ! dpkg -l | grep -q ca-certificates; then
     sudo apt-get update
-    sudo apt-get install -y lsb-release ca-certificates vim
+    sudo apt-get install -y lsb-release ca-certificates sshpass vim
 fi
 
 # vagrant 계정이 존재하지 않으면 vagrant_useradd.sh 스크립트 실행
@@ -56,7 +56,7 @@ if ! id "$user_name" &>/dev/null; then
 fi
 
 if [ "$HOSTNAME" == "ansible" ]; then
-    #ssh-keygen -t rsa -b 2048 -C "deployment" -f /home/$user_name/.ssh/id_rsa -N ""
+    #ssh-keygen -t rsa -b 2048 -C "deployment" -f /home/vagrant/.ssh/id_rsa -N ""
     if [ ! -f "/home/$user_name/.ssh/id_rsa" ]; then
         sudo mkdir -m 700 /home/$user_name/.ssh
         # 함수 호출하여 id_rsa 파일 생성
@@ -93,6 +93,14 @@ if [ ! -f "/home/$user_name/.ssh/authorized_keys" ]; then
     add_ssh_authorized_key
     sudo echo "$user_name:$user_name" | chpasswd
     sudo chown -R "$user_name:$user_name" /home/$user_name/.ssh
+fi
+
+if [ "$HOSTNAME" == "ansible" ]; then
+    hosts=("172.19.0.11" "172.19.0.12")
+    password="$user_name"
+    for host in "${hosts[@]}"; do
+        echo "$password" | sshpass -p "$password" ssh-copy-id "$user_name@$host"
+    done
 fi
 
 
